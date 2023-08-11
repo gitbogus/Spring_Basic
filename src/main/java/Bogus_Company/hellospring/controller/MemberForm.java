af07303f8419e960c48eecc8ecbebbaa2cde9f2b
gitbogus <bogus0115@naver.com>
2023-08-11 16:47:06 +0900

    memberList add

---
+++ src/main/java/Bogus_Company/hellospring/controller/MemberForm.java
@@ -0,0 +1,12 @@
+package Bogus_Company.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
