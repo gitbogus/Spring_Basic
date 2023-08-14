9d0f53cc9e6cc1303c1c66ad2f6d6adac6ff7603
gitbogus <bogus0115@naver.com>
2023-08-14 20:50:30 +0900

    JDBC Connect

--- src/main/java/Bogus_Company/hellospring/service/SpringConfig.java
+++ src/main/java/Bogus_Company/hellospring/service/SpringConfig.java
@@ -1,20 +1,33 @@
 package Bogus_Company.hellospring.service;
 
+import Bogus_Company.hellospring.reposirory.JdbcMemberRepository;
 import Bogus_Company.hellospring.reposirory.MemberRepository;
 import Bogus_Company.hellospring.reposirory.MemoryMemberRepository;
 import Bogus_Company.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
+
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
